ac2ffd05147b08dbeb7037baf59afe60229042c0
Michał Kątnik <michal.katnik91@gmail.com>
2023-11-12 19:50:47 +0100

    Base ConfigParser implementation added

--- src/main.go
+++ src/main.go
@@ -1,9 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/rideee/go-universal-health-check/src/utils"
+)
 
 func main() {
 
-	fmt.Println("Hello, World!")
+	exec, _ := os.Executable()
+	baseDir := filepath.Dir(exec)
+	configDir := filepath.Join(baseDir, "config")
+	configServersFile := filepath.Join(configDir, "servers.json")
+
+	config := utils.NewConfigParser(configServersFile)
+
+	fmt.Println("\nUniversal Health Check (Go implementation) made by Michał Kątnik (github.com/rideee)")
+	fmt.Printf("Config directory location: %s\n\n", configDir)
+
+	// Main loop.
+	for _, srv := range config.ServersConfigData.Servers {
+		fmt.Printf("Running checks for %s\n", srv.Name)
+	}
 
 }
--- src/utils/ConfigParser.go
+++ src/utils/ConfigParser.go
@@ -1,1 +1,41 @@
 package utils
+
+import (
+	"encoding/json"
+	"os"
+
+	"github.com/rideee/go-universal-health-check/src/models"
+)
+
+type ConfigParser struct {
+	ServersConfigFile string
+	ServersConfigData models.ServersConfig
+	ServerTypesMap    map[string]models.ServerType
+}
+
+// Constructor.
+func NewConfigParser(serversConfigFile string) *ConfigParser {
+	cp := new(ConfigParser)
+	cp.ServersConfigFile = serversConfigFile
+
+	// Import json file.
+	serversFileData, err := os.ReadFile(serversConfigFile)
+	if err != nil {
+		panic(err)
+	}
+
+	// Unmarshal json.
+	err = json.Unmarshal(serversFileData, &cp.ServersConfigData)
+	if err != nil {
+		panic(err)
+	}
+
+	// Make a server types object map[string]models.ServerType.
+	srvTypesMap := make(map[string]models.ServerType)
+	for _, srvType := range cp.ServersConfigData.ServerTypes {
+		srvTypesMap[srvType.Name] = srvType
+	}
+	cp.ServerTypesMap = srvTypesMap
+
+	return cp
+}
