7231e0da2136ffc14b09de0f5423994b931fa60b
Michał Kątnik <michal.katnik91@gmail.com>
2023-11-12 21:39:12 +0100

    fixed field name bug in json file and time measuring added

--- src/main.go
+++ src/main.go
@@ -4,11 +4,15 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"time"
 
 	"github.com/rideee/go-universal-health-check/src/utils"
 )
 
 func main() {
+
+	// Measuring execution time.
+	startTime := time.Now()
 
 	exec, _ := os.Executable()
 	baseDir := filepath.Dir(exec)
@@ -18,11 +22,18 @@ func main() {
 	config := utils.NewConfigParser(configServersFile)
 
 	fmt.Println("\nUniversal Health Check (Go implementation) made by Michał Kątnik (github.com/rideee)")
-	fmt.Printf("Config directory location: %s\n\n", configDir)
+	fmt.Printf("Config directory location: %s\n", configDir)
 
 	// Main loop.
 	for _, srv := range config.ServersConfigData.Servers {
-		fmt.Printf("Running checks for %s\n", srv.Name)
+		srvTypeObject := config.ServerTypesMap[srv.ServerType]
+		fmt.Printf("\nRunning checks for %s\n", srv.Name)
+		fmt.Printf("Type: %s\n", srv.ServerType)
+		fmt.Printf("Method: %s, %s:%s logging as %s\n", srvTypeObject.Method, srv.IP, srv.Port, srv.Username)
+		fmt.Printf("Command:\n%s\n", srvTypeObject.Command)
 	}
 
+	// Elapsed time.
+	elapsedTime := time.Since(startTime)
+	fmt.Printf("\n\n(Execution time: %s)\n\n", elapsedTime)
 }
