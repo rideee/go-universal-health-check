aff954b693d643861fdadf6792ed881da81642a2
Michał Kątnik <michal.katnik91@gmail.com>
2023-11-13 17:20:36 +0100

    channels added

--- src/main.go
+++ src/main.go
@@ -6,6 +6,7 @@ import (
 	"path/filepath"
 	"time"
 
+	"github.com/rideee/go-universal-health-check/src/models"
 	"github.com/rideee/go-universal-health-check/src/utils"
 )
 
@@ -14,6 +15,7 @@ func main() {
 	// Measuring execution time.
 	startTime := time.Now()
 
+	// Set Paths.
 	exec, _ := os.Executable()
 	baseDir := filepath.Dir(exec)
 	configDir := filepath.Join(baseDir, "config")
@@ -24,13 +26,33 @@ func main() {
 	fmt.Println("\nUniversal Health Check (Go implementation) made by Michał Kątnik (github.com/rideee)")
 	fmt.Printf("Config directory location: %s\n", configDir)
 
-	// Main loop.
+	// Initialize channels slice.
+	var channels []chan string
+
+	// Goroutine main loop.
 	for _, srv := range config.ServersConfigData.Servers {
-		srvTypeObject := config.ServerTypesMap[srv.ServerType]
-		fmt.Printf("\nRunning checks for %s\n", srv.Name)
-		fmt.Printf("Type: %s\n", srv.ServerType)
-		fmt.Printf("Method: %s, %s:%s logging as %s\n", srvTypeObject.Method, srv.IP, srv.Port, srv.Username)
-		fmt.Printf("Command:\n%s\n", srvTypeObject.Command)
+
+		// Make a new channel for every iterration of the loop and apped it to channels slice.
+		channel := make(chan string)
+		channels = append(channels, channel)
+
+		// Run goroutine annonymous function.
+		go func(channel chan string, srv models.Server) {
+
+			srvTypeObject := config.ServerTypesMap[srv.ServerType]
+			output := fmt.Sprintf("\nRunning checks for %s\n", srv.Name)
+			output += fmt.Sprintf("Type: %s\n", srv.ServerType)
+			output += fmt.Sprintf("Method: %s, %s:%s logging as %s\n", srvTypeObject.Method, srv.IP, srv.Port, srv.Username)
+			output += fmt.Sprintf("Command:\n%s\n", srvTypeObject.Command)
+
+			channel <- output
+
+		}(channel, srv)
+	}
+
+	// Collect data from all channels in correct order (FIFO).
+	for _, ch := range channels {
+		fmt.Println(<-ch)
 	}
 
 	// Elapsed time.
