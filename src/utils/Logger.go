4f259e51eafc9080765c61aa680641f46ed564b9
Michał Kątnik <michal.katnik91@gmail.com>
2023-11-18 17:48:34 +0100

    Separate functions were migrated to Logger methods

--- src/utils/Logger.go
+++ src/utils/Logger.go
@@ -2,50 +2,73 @@ package utils
 
 import "fmt"
 
+type Logger struct {
+	DebugMode bool
+}
+
+// Constructor.
+func NewLogger(debugMode bool) *Logger {
+	l := new(Logger)
+	l.DebugMode = debugMode
+	return l
+}
+
 // Same as fmt.Println, but also writes the message to the log file.
 // TODO: store logs in a file...
-func Println(msg ...any) {
+func (l *Logger) Println(msg ...any) {
 	fmt.Println(msg...)
 }
 
 // Same as fmt.Printf, but also writes the message to the log file.
 // TODO: store logs in a file...
-func Printf(format string, v ...any) {
+func (l *Logger) Printf(format string, v ...any) {
 	fmt.Printf(format, v...)
 }
 
 // Same as fmt.Println, but also writes the message to the log file.
 // TODO: store logs in a file...
-func Debug(msg ...any) {
+func (l *Logger) Debug(msg ...any) {
+	if !l.DebugMode {
+		return
+	}
+	fmt.Print("DEBUG: ")
 	fmt.Println(msg...)
 }
 
 // Same as fmt.Printf, but also writes the message to the log file.
 // TODO: store logs in a file...
-func Debugf(format string, v ...any) {
+func (l *Logger) Debugf(format string, v ...any) {
+	if !l.DebugMode {
+		return
+	}
+	fmt.Print("DEBUG: ")
 	fmt.Printf(format, v...)
 }
 
 // Same as fmt.Println, but also writes the message to the log file.
 // TODO: store logs in a file...
-func Warning(msg ...any) {
+func (l *Logger) Warning(msg ...any) {
+	fmt.Print("WARNING: ")
 	fmt.Println(msg...)
 }
 
 // Same as fmt.Printf, but also writes the message to the log file.
 // TODO: store logs in a file...
-func Warningf(format string, v ...any) {
+func (l *Logger) Warningf(format string, v ...any) {
+	fmt.Print("WARNING: ")
 	fmt.Printf(format, v...)
 }
 
 // Same as fmt.Println, but also writes the message to the log file.
 // TODO: store logs in a file...
-func Error(msg ...any) {
+func (l *Logger) Error(msg ...any) {
+	fmt.Print("ERROR: ")
 	fmt.Println(msg...)
 }
 
 // Same as fmt.Printf, but also writes the message to the log file.
 // TODO: store logs in a file...
-func Errorf(format string, v ...any) {
+func (l *Logger) Errorf(format string, v ...any) {
+	fmt.Print("ERROR: ")
 	fmt.Printf(format, v...)
 }
