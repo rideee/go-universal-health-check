cc6518bbe2e41ba2ea5188c97f37f280477155b0
Michał Kątnik <michal.katnik91@gmail.com>
2023-11-18 16:58:20 +0100

    Base funcs added

--- src/utils/Logger.go
+++ src/utils/Logger.go
@@ -1,1 +1,51 @@
 package utils
+
+import "fmt"
+
+// Same as fmt.Println, but also writes the message to the log file.
+// TODO: store logs in a file...
+func Println(msg ...any) {
+	fmt.Println(msg...)
+}
+
+// Same as fmt.Printf, but also writes the message to the log file.
+// TODO: store logs in a file...
+func Printf(format string, v ...any) {
+	fmt.Printf(format, v...)
+}
+
+// Same as fmt.Println, but also writes the message to the log file.
+// TODO: store logs in a file...
+func Debug(msg ...any) {
+	fmt.Println(msg...)
+}
+
+// Same as fmt.Printf, but also writes the message to the log file.
+// TODO: store logs in a file...
+func Debugf(format string, v ...any) {
+	fmt.Printf(format, v...)
+}
+
+// Same as fmt.Println, but also writes the message to the log file.
+// TODO: store logs in a file...
+func Warning(msg ...any) {
+	fmt.Println(msg...)
+}
+
+// Same as fmt.Printf, but also writes the message to the log file.
+// TODO: store logs in a file...
+func Warningf(format string, v ...any) {
+	fmt.Printf(format, v...)
+}
+
+// Same as fmt.Println, but also writes the message to the log file.
+// TODO: store logs in a file...
+func Error(msg ...any) {
+	fmt.Println(msg...)
+}
+
+// Same as fmt.Printf, but also writes the message to the log file.
+// TODO: store logs in a file...
+func Errorf(format string, v ...any) {
+	fmt.Printf(format, v...)
+}
